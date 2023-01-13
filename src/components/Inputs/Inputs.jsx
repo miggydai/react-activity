b206940d1bab402a59398fd657d35581735edf05
Miguel Emmanuel Paredes Dailisan <mepdailisan@addunet.ph>
2023-01-13 19:21:56 +0800

    first commit

---
+++ src/components/Inputs/Inputs.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Inputs = () => {
+    return (
+        <>
+        <div className="">
+             <b>Name</b> - price 
+             </div>
+        </>
+
+    )
+}
+
+export default Inputs
