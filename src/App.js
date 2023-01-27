1d544901572443f2abc93d3aaf6d9ad9538057d5
Miguel Emmanuel Paredes Dailisan <mepdailisan@addunet.ph>
2023-01-27 19:18:04 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { Inputs } from "./components/ComponentIndex";
+
+const expenses = [
+  {id:1,name: "phone" ,price:1000},
+  {id:2,name: "phweafsone" ,price:1000},
+  {id:3,name: "das" ,price:1000},
+  {id:4,name: "das" ,price:1000},
+  {id:5,name: "csed" ,price:1000},
+];
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     {expenses.map((e) => (
+        <Inputs id={e.id} name={e.name} price= {e.price}/>
+      ))}
+    
+    </>
   );
 }
 
--- src/components/Inputs/Inputs.jsx
+++ src/components/Inputs/Inputs.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { useState } from 'react';
+import "./Inputs.css"
 
-const Inputs = () => {
+
+
+
+const Inputs = (props) => {
+    const [deleted, setDeleted] = useState();
     return (
         <>
-        <div className="">
-             <b>Name</b> - price 
+       
+        <div className="container">
+            <div><b>{props.name}</b> - {props.price}</div>
+              
+             
+             <div>
+                 <button className="button">Delete</button>
              </div>
+             </div>
+        
         </>
 
     )
